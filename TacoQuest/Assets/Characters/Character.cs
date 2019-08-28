605cf15a6422688da789a6e9a3f5c87020b7f3be
caposcar1998 <42816786+caposcar1998@users.noreply.github.com>
2019-08-27 21:29:33 -0500

    Create Character.cs
    Creating class with getters and setters

---
+++ TacoQuest/Assets/Characters/Character.cs
@@ -0,0 +1,23 @@
+using System;
+
+public abstract class Character{
+
+    private String name{
+        get;
+        set;
+    }
+    private double life{
+        get;
+        set;
+    }
+    private double atackPower{
+        get;
+        set;
+    }
+    private double defensePower{
+        get;
+        set;
+    }
+
+
+}
