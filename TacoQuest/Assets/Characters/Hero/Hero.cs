fe1a0c504606d431852a32500e5ad940f64e8c54
caposcar1998 <42816786+caposcar1998@users.noreply.github.com>
2019-08-31 22:53:23 -0500

    Update Hero.cs
    No interfaces

--- TacoQuest/Assets/Characters/Hero/Hero.cs
+++ TacoQuest/Assets/Characters/Hero/Hero.cs
@@ -1,20 +1,13 @@
 //To work in unity
 using UnityEngine;
 //MonoBehavior is needed in all classes
-public class Hero : MonoBehaviour, Character, BasicAccion {
+public class Hero : MonoBehaviour{
    
     //Constructor
     public Hero(){}
 
    //Getters and setters of interfaces
-    string Character.name{get; set;}
-    double Character.life{get; set;}
-    double Character.atackPower{get; set;}
-    double Character.defensePower{get; set;}
-
-    void BasicAccion.meleeAtack(Character a){}
-    void BasicAccion.rangeAtack(Character a){}
-    void BasicAccion.move(Character a){}
+   
 
     
     
