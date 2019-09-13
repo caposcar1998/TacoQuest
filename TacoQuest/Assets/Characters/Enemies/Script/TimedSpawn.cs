16758e8915328b4db482f9f4495a1d9905c71fca
caposcar1998 <42816786+caposcar1998@users.noreply.github.com>
2019-09-12 20:58:47 -0500

    Added static spawner
    The spawner create a character every certain time

---
+++ TacoQuest/Assets/Characters/Enemies/Script/TimedSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedSpawn : MonoBehaviour
+{
+
+    public GameObject spawnee;
+    public bool stop = false;
+    public float time;
+    public float delay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemie", time, delay);
+    }
+
+    public void SpawnEnemie(){
+        Instantiate(spawnee, transform.position, transform.rotation);
+        if (stop){
+            CancelInvoke("SpawnEnemie");
+        }
+    }
+
+}
