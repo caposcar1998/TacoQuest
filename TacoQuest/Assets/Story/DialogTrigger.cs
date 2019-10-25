e972d22c19bd32f2896ff75a5f812b6690d82acb
Manue Ortiz Hernández <manuoh09@gmail.com>
2019-10-25 14:58:53 -0500

    Mapa Completo
    Fírmala Gio, fírmala

---
+++ TacoQuest/Assets/Story/DialogTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggeEnter2D(Collider2D other){
+        if(other.CompareTag("Player")){
+            print("dv");
+        }
+    }
+}
