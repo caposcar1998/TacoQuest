a39a8848551fda3d706f981dc828ca6c1ea8388b
caposcar1998 <42816786+caposcar1998@users.noreply.github.com>
2019-10-24 22:46:25 -0500

    Cambios bn pinches perro
    Los mnuros se la comen entera

--- TacoQuest/Assets/Characters/Hero/Scripts_hero/Bullet.cs
+++ TacoQuest/Assets/Characters/Hero/Scripts_hero/Bullet.cs
@@ -6,9 +6,10 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 20.0f;
     public Rigidbody2D rb;
-    private float lifetime = 2.5f;
+    private float lifetime = 0.5f;
 
     public float throath;
+    public float knockTime;
         void Start()
     {
         Destroy(gameObject, lifetime);
@@ -30,35 +31,31 @@ public class Bullet : MonoBehaviour
     
     }
 
-    void FixedUpdate(){
-        //rb.AddForce(Vector3.forward * 100);
-    }
+    
 
     void OnTriggerEnter2D(Collider2D object_hitted){
 
        if (object_hitted.gameObject.CompareTag("Enemy")){ 
            object_hitted.GetComponent<Enemie_stats>().TakeDamage(.5f);
-            Destroy(gameObject);
             Rigidbody2D enemy = object_hitted.GetComponent<Rigidbody2D>();
             if (enemy != null){
                 enemy.isKinematic = false;
                 Vector2 difference = enemy.transform.position - transform.position;
                 difference = difference.normalized * throath;
                 enemy.AddForce(difference, ForceMode2D.Impulse);
-                enemy.isKinematic = true;
-                //StartCoroutine(knockCo(enemy));
+                StartCoroutine(KnockCo(enemy));
             }
+            
        }
-       
     }
     // Update is called once per frame
-    //IEnumerator KnockCo(Rigidbody2D enemy){
-    //    if(enemy != null){
-    //        yield return new WaitForSeconds(knockTime);
-    //        enemy.velocity = Vector2.zero;
-    //        enemy.isKinematic = true;
-    //    }       
-    //}
+    private IEnumerator KnockCo(Rigidbody2D enemy){
+        if(enemy != null){
+            yield return new WaitForSeconds(knockTime);
+            enemy.velocity = Vector2.zero;
+            enemy.isKinematic = true;
+        }       
+    }
 
 
 }
